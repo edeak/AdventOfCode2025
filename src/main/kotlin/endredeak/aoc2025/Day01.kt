b56dc8e6b14fbde1eb57164a73510ee7c3b20061
Endre Deak <profendre@gmail.com>
2025-12-02 10:29:46 -0800

    2025 day 1

--- src/main/kotlin/endredeak/aoc2025/Day01.kt
+++ src/main/kotlin/endredeak/aoc2025/Day01.kt
@@ -1,15 +1,35 @@
 package endredeak.aoc2025
 
 fun main() {
-    solve("") {
-        val input = lines
+    solve("Secret Entrance") {
+        val input = lines.map { it.first() to it.drop(1).toInt() }
 
-        part1(-1) {
-            -1
+        part1(1011) {
+            var dial = 50
+            var count = 0
+            input
+                .forEach { (dir, amount) ->
+                    val d = if (dir == 'R') 1 else -1
+                    dial += d * amount
+                    dial = dial.mod(100)
+                    if (dial == 0) count++
+                }
+            count
         }
 
-        part2(-1) {
-            -1
+        part2(5937) {
+            var dial = 50
+            var count = 0
+            input
+                .forEach { (dir, amount) ->
+                    val d = if (dir == 'R') 1 else -1
+                    repeat(amount) {
+                        dial += d
+                        dial = dial.mod(100)
+                        if (dial == 0) count++
+                    }
+                }
+            count
         }
     }
 }
