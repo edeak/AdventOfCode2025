d057f1fb6eb901f81a8fb45954917cd082d33c13
Endre Deak <deak@csdisco.com>
2025-12-02 22:40:29 -0800

    day 3

--- src/main/kotlin/endredeak/aoc2025/Day03.kt
+++ src/main/kotlin/endredeak/aoc2025/Day03.kt
@@ -1,15 +1,20 @@
 package endredeak.aoc2025
 
 fun main() {
-    solve("") {
+    solve("Lobby") {
         val input = lines
 
-        part1(-1) {
-            -1
-        }
+        fun maxJoltage(bank: String, n: Int): String =
+            if (bank.isNotEmpty() && n != 0)
+                bank
+                    .dropLast(n - 1)
+                    .withIndex()
+                    .maxBy { it.value }
+                    .let { max -> max.value + maxJoltage(bank.drop(max.index + 1), n - 1) }
+            else ""
 
-        part2(-1) {
-            -1
-        }
+        part1(17263) { input.sumOf { maxJoltage(it, 2).toLong() } }
+
+        part2(3121910778619) { input.sumOf { maxJoltage(it, 12).toLong() } }
     }
 }
