cfe6027ea35c95490b9d13d4fb3c0856e093939b
Endre Deak <deak@csdisco.com>
2025-12-03 23:05:24 -0800

    day 4

--- src/main/kotlin/endredeak/aoc2025/Day04.kt
+++ src/main/kotlin/endredeak/aoc2025/Day04.kt
@@ -1,15 +1,29 @@
 package endredeak.aoc2025
 
-fun main() {
-    solve("") {
-        val input = lines
+import endredeak.aoc2025.lib.utils.Coord
+import endredeak.aoc2025.lib.utils.toGrid
 
-        part1(-1) {
-            -1
+fun main() {
+    solve("Printing Department") {
+        val input = lines.toGrid { it }
+
+        fun Map.Entry<Coord, Char>.removable() =
+            this.value == '@' && this.key.neighbours(true).count { c -> input[c] == '@'} < 4
+
+        part1 {
+            input.count { it.removable() }
         }
 
-        part2(-1) {
-            -1
+        part2 {
+            var sum = 0
+            while(true) {
+                input
+                    .filter { it.removable() }
+                    .takeIf { it.isNotEmpty() }
+                    ?.forEach { (c, _) -> input[c] = '.'; sum++ }
+                    ?: break
+            }
+            sum
         }
     }
 }
