79c2d13edfa25b40114f884023bf85b711d2b817
Endre Deak <deak@csdisco.com>
2025-12-04 21:50:47 -0800

    day 5

--- src/main/kotlin/endredeak/aoc2025/Day05.kt
+++ src/main/kotlin/endredeak/aoc2025/Day05.kt
@@ -1,15 +1,37 @@
 package endredeak.aoc2025
 
+import kotlin.math.max
+
 fun main() {
     solve("") {
         val input = lines
+            .filter { it.isNotBlank() }
+            .partition { it.contains("-") }
+            .let { (f, a) ->
+                f.map { it.split("-").let { (a, b) -> LongRange(a.toLong(), b.toLong()) } } to
+                        a.map { it.toLong() }
+            }
 
-        part1(-1) {
-            -1
+        part1(896) {
+            input.let { (fresh, available) ->
+                available.count { a -> fresh.any { f -> a in f } }
+            }
         }
 
-        part2(-1) {
-            -1
+        part2(346240317247002) {
+            input.let { (fresh, _) ->
+                var rem = fresh.sortedBy { it.first }
+                var count = 0L
+                var max = 0L
+                while(rem.isNotEmpty()) {
+                    val f = rem.first()
+                    val min = max(f.first, max+1)
+                    max = rem.takeWhile { it.first <= f.last }.maxOf { it.last }
+                    count += (max-min)+1
+                    rem = rem.filter { it.last > max }
+                }
+                count
+            }
         }
     }
 }
