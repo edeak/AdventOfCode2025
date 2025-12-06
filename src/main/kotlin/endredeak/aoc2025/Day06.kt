ac12b3e0cda51df58bfbb8b9c0956932d16e48b0
Endre Deak <deak@csdisco.com>
2025-12-06 15:05:38 -0800

    Day 6

--- src/main/kotlin/endredeak/aoc2025/Day06.kt
+++ src/main/kotlin/endredeak/aoc2025/Day06.kt
@@ -1,15 +1,54 @@
 package endredeak.aoc2025
 
-fun main() {
-    solve("") {
-        val input = lines
+import endredeak.aoc2025.lib.utils.product
+import endredeak.aoc2025.lib.utils.transpose
 
-        part1(-1) {
-            -1
+fun main() {
+    solve("Trash Compactor") {
+        fun List<Long>.calc(op: Char) =
+            when (op) {
+                '*' -> this.product()
+                else -> this.sum()
+            }
+
+        part1 {
+            lines
+                .map { l -> l.split(" ").filter { it.isNotBlank() } }
+                .transpose()
+                .sumOf { p ->
+                    p.dropLast(1)
+                        .map { it.toLong() }
+                        .calc(p.last().first())
+                }
         }
 
-        part2(-1) {
-            -1
+        part2 {
+            var raw = lines
+                .maxOf { it.length }
+                .let { max ->
+                    lines
+                        .map { it.padEnd(max, ' ').toCharArray().reversed().toList() }
+                        .transpose()
+                }
+
+            val groups = mutableListOf<List<List<Char>>>()
+            var g = mutableListOf<List<Char>>()
+            while (raw.isNotEmpty()) {
+                if (raw[0].any { !it.isWhitespace() }) {
+                    g.add(raw[0])
+                } else {
+                    groups.add(g).also { g = mutableListOf() }
+                }
+                raw = raw.drop(1)
+            }
+            groups.add(g)
+
+            groups
+                .sumOf { line ->
+                    line
+                        .map { it.filter { c -> c.isDigit() }.joinToString("").toLong() }
+                        .calc(line.last().last())
+                }
         }
     }
 }
