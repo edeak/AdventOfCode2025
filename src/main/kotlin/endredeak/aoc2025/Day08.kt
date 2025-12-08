c7cd8e6cc2db3c205f13009cb23e8d029189fa0e
Endre Deak <deak@csdisco.com>
2025-12-07 22:36:34 -0800

    Day 8

--- src/main/kotlin/endredeak/aoc2025/Day08.kt
+++ src/main/kotlin/endredeak/aoc2025/Day08.kt
@@ -1,15 +1,66 @@
 package endredeak.aoc2025
 
-fun main() {
-    solve("") {
-        val input = lines
+import endredeak.aoc2025.lib.utils.product
+import endredeak.aoc2025.lib.utils.productOf
+import kotlin.math.pow
+import kotlin.math.sqrt
+import kotlin.time.measureTimedValue
 
-        part1(-1) {
-            -1
+fun main() {
+    solve("Playground") {
+        data class C(val x: Double, val y: Double, val z: Double) {
+            fun dist(other: C): Double =
+                sqrt((x - other.x).pow(2) + (y - other.y).pow(2) + (z - other.z).pow(2))
         }
 
-        part2(-1) {
-            -1
+        val input = lines
+            .map { it.split(",").let { (x, y, z) -> C(x.toDouble(), y.toDouble(), z.toDouble()) } }
+
+        val distances = input
+            .flatMap { c -> input.filter { it != c }.map { setOf(c, it) } }
+            .distinct()
+            .sortedBy { s -> s.take(2).let { (a, b) -> a.dist(b) } }
+
+
+        fun iterate(
+            cond: () -> Boolean,
+            circuits: MutableSet<MutableSet<C>>,
+            callback: () -> Unit
+        ): Pair<Set<C>, Set<Set<C>>> =
+            distances
+                .toMutableList()
+                .let { dists ->
+                    var j: Set<C> = emptySet()
+                    while (cond()) {
+                        j = dists.first()
+                        val matches = circuits.filter { it.intersect(j).isNotEmpty() }
+
+                        circuits.removeAll(matches)
+                        circuits.add(matches.flatMap { it }.toMutableSet())
+
+                        dists.removeAt(0)
+                        callback()
+                    }
+
+                    j to circuits
+                }
+
+        part1 {
+            var c = 0
+            val circuits = input.map { mutableSetOf(it) }.toMutableSet()
+            iterate({ c < 1000 }, circuits) { c++ }
+                .second
+                .map { it.size.toLong() }.sorted()
+                .reversed()
+                .take(3)
+                .product()
+        }
+
+        part2 {
+            val circuits = input.map { mutableSetOf(it) }.toMutableSet()
+            iterate({ circuits.size != 1 }, circuits) {}
+                .first
+                .productOf { it.x.toLong() }
         }
     }
 }
