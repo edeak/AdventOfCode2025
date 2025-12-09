cec2ea05901f64c7758989ed9f44743d00d04659
Endre Deak <deak@csdisco.com>
2025-12-09 00:22:15 -0800

    Day 9

--- src/main/kotlin/endredeak/aoc2025/Day09.kt
+++ src/main/kotlin/endredeak/aoc2025/Day09.kt
@@ -1,15 +1,43 @@
 package endredeak.aoc2025
 
-fun main() {
-    solve("") {
-        val input = lines
+import endredeak.aoc2025.lib.utils.Coord
+import kotlin.math.max
+import kotlin.math.min
 
-        part1(-1) {
-            -1
+fun main() {
+    solve("Movie Theater") {
+        fun Pair<Coord, Coord>.sort(): Pair<Coord, Coord> = Pair(
+            Coord(min(first.x, second.x), min(first.y, second.y)),
+            Coord(max(first.x, second.x), max(first.y, second.y))
+        )
+
+        val coords: List<Coord> = lines
+            .map { it.split(",").let { (x, y) -> Coord(x.toInt(), y.toInt()) } }
+
+        val pairs = coords
+            .flatMapIndexed { y, c -> coords.drop(y + 1).map { c to it } }
+            .toSet()
+            .map { it.sort() }
+
+        val edges = coords
+            .plus(coords.first())
+            .zipWithNext()
+            .map { it.sort() }
+
+        part1 {
+            pairs.maxOf { (p1, p2) ->
+                (p2.y - p1.y + 1L) * (p2.x - p1.x + 1L)
+            }
         }
 
-        part2(-1) {
-            -1
+        part2 {
+            pairs.filter { (p1, p2) ->
+                edges.none { (l1, l2) ->
+                    p1.x < l2.x && p2.x > l1.x && p1.y < l2.y && p2.y > l1.y
+                }
+            }.maxOf { (p1, p2) ->
+                (p2.y - p1.y + 1L) * (p2.x - p1.x + 1L)
+            }
         }
     }
 }
