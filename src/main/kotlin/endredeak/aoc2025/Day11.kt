cf72b6de256336e7aa901dda765df35e6f9b0f1d
Endre Deak <deak@csdisco.com>
2025-12-10 22:50:27 -0800

    Day 11

--- src/main/kotlin/endredeak/aoc2025/Day11.kt
+++ src/main/kotlin/endredeak/aoc2025/Day11.kt
@@ -1,15 +1,21 @@
 package endredeak.aoc2025
 
 fun main() {
-    solve("") {
-        val input = lines
+    solve("Reactor") {
+        val input = lines.associate { it.split(": ").let { (k, v) -> k to v.split(" ") } }
 
-        part1(-1) {
-            -1
-        }
+        fun calc(src: String, dst: String) : Long =
+            with(mutableMapOf<String, Long>()) {
+                return DeepRecursiveFunction { key ->
+                    if (key == dst) 1L else getOrPut(key) { input[key]?.sumOf { callRecursive(it) } ?: 0 }
+                }(src)
+            }
 
-        part2(-1) {
-            -1
+        part1 { calc("you", "out") }
+
+        part2 {
+            calc("svr", "fft") * calc("fft", "dac") * calc("dac", "out") +
+                    calc("svr", "dac") * calc("dac", "fft") * calc("fft", "out")
         }
     }
 }
